6c83941f0976353c20d2031b5b06ae430ba74258
sure harish <hsure@nisum.com>
2017-10-30 16:23:54 +0530

    dskjfhkjd

---
+++ src/main/java/com/example/nisum/SpringBootDemo/Service/ProductService.java
@@ -0,0 +1,16 @@
+package com.example.nisum.SpringBootDemo.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+	public String getAllProducts()
+	{
+		return "all the product's";
+	}
+
+	public String getProduct()
+	{
+		return "all the product's";
+	}
+}
